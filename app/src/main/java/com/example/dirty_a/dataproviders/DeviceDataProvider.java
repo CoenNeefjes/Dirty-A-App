d4afe34e8762947bd057e302efdf46db5768d56e
Coen Neefjes <coenneefjes@hotmail.com>
2019-04-13 00:57:21 +0200

    made singleton dataprovider class for devices

--- app/src/main/java/com/example/dirty_a/dataproviders/DeviceDataProvider.java
+++ app/src/main/java/com/example/dirty_a/dataproviders/DeviceDataProvider.java
@@ -1,4 +1,18 @@
 package com.example.dirty_a.dataproviders;
 
+import com.example.dirty_a.model.Device;
+
+import java.util.List;
+
 public class DeviceDataProvider {
+
+    private static final DeviceDataProvider deviceDataProvider = new DeviceDataProvider();
+
+    private List<Device> devices;
+
+    private DeviceDataProvider() {}
+
+    public static DeviceDataProvider getInstance() {
+        return deviceDataProvider;
+    }
 }
